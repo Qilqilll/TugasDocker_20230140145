b63ddd034c6706d3efdbc37ab77ddeda9a09d1b5
Qilqilll <ASUS@LAPTOP-3HJQCS6H>
2026-04-14 09:33:17 +0700

    membuat folder dan file (controller dan model)

---
+++ src/test/java/com/tugas/deploy/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tugas.deploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
